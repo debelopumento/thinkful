8ef9c5e996a3247e560f882343e104a500fa4429
debelopumento <imdiye@Gmail.com>
2016-12-24 23:00:09 -0800

    Add files via upload
    Added averageSentenceLength and AverageWordLength functions.

--- textAnalyzer/app.js
+++ textAnalyzer/app.js
@@ -7,22 +7,29 @@ function wordCounter(text){
 }
 
 
-function tokenizer(text){
-  return text.toLowerCase().match(/[a-zA-Z0-9]/gm);
-  
+function totalLetterCounter(text){
+  var letters=text.toLowerCase().match(/[a-zA-Z0-9]/gm);
+  var totalLetterCount=letters.length;
+  return totalLetterCount;
 }
 
+function avSentenceLengthCal(text, wordCount){
+  var sentenceCount=(text.match(/(\.|\?|!|")/gm)).length;
+  var avSentenceLength=(wordCount / sentenceCount).toFixed(2);
+  return avSentenceLength;
+}
 
 function reportOnText(text){
-  var tokens=tokenizer(text);
   var wordCount=wordCounter(text);
-  
+  var totalLetterCount=totalLetterCounter(text);
+  var averageWordLength=(totalLetterCount / wordCount).toFixed(2);
+  var avSentenceLength=avSentenceLengthCal(text, wordCount);
   
   var textReport=$('.js-text-report');
   textReport.removeClass('hidden');
   textReport.find('.js-wordCount').text(wordCount);
-  
-  
+  textReport.find('.js-averageWordLength').text(averageWordLength + " letters");
+  textReport.find('.js-averageSentenceLength').text(avSentenceLength + " words");
 }
 
 
