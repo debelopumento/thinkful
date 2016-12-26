823193076f67fd474098e66dfdbe00d84dda54ed
debelopumento <imdiye@Gmail.com>
2016-12-26 12:44:01 -0800

    Updated delete buttons

--- shoppingList/app.js
+++ shoppingList/app.js
@@ -3,18 +3,19 @@ var state = {
   items: []
 };
 
-items = ["apple", "orange", "coffee"];
+items = ["apples", "oranges", "milk", "bread"];
 
 
 function returnItemHtml(item){
   return ('<li>' +
-  '<span class="shopping-item">' + item +
+  '<span class="shopping-item js-shopping-item">' + 
+  item +
   '</span>' + 
     '<div class="shopping-item-controls">' +
-      '<button class="js-shopping-item-toggle">' +
+      '<button class="js-shopping-item-toggle js-check">' +
         '<span class="button-label">check</span>' +
       '</button>' +
-      '<button class="js-shopping-item-delete">' +
+      '<button class="js-shopping-item-delete js-delete-button">' +
         '<span class="button-label">delete</span>' +
       '</button>' +
     '</div>' +
@@ -38,7 +39,7 @@ function resetList(){
   $('.js-items-div').html("<ul class='shopping-list js-shopping-list'></ul>");
 }
 
-function renderItemList(items, newInput){
+function renderItemList(items){
   console.log("item list is: ");
   //reset list
   resetList();
@@ -49,18 +50,33 @@ function renderItemList(items, newInput){
   }  
 }
 
-$('.js-shopping-item-delete').click(function(event){
-    //remove this li
-    
-});
+
+function deleteOnClick(){
+  $('.js-delete-button').click(function(event){
+    var itemID=$(this.closest('li')).find('.js-shopping-item').val();
+    console.log("deleted item is ", itemID);
+    this.closest('li').remove();
+    delete items[itemID];
+  });
+}
+
+function checkOnClick(){
+  $('.js-check').click(function(event){
+    console.log("check this item");
+    this.closest('span').removeClass('shopping-item__checked');
+    $(event.currentTarget).addClass('shopping-item__checked');
+  });
+}
+
 
 $(function userInput(){
   $('.js-form').submit(function(event){
     event.preventDefault();
-    
     var newInput = $(this).find('#user-newItemInput').val();
     console.log("input new item is: " + newInput);
     pushItem(state, items, newInput);
-    renderItemList(items, newInput);
+    renderItemList(items);
     });
+  deleteOnClick();
+  checkOnClick();
 })
