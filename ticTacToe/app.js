7b7a9ccfe392f940e3eadd4f65da4608abb0417b
debelopumento <imdiye@Gmail.com>
2017-01-04 10:47:49 -0800

    added finishGame logic and alert

--- ticTacToe/app.js
+++ ticTacToe/app.js
@@ -46,9 +46,32 @@ function playGame() {
 		}
 		else {
 			$(event.currentTarget).toggleClass('O');
-			row[currentRow][currentColumn] = "O"
+			row[currentRow][currentColumn] = "O";
 		}
 		console.log("currentRow", currentRow, "currentColumn", currentColumn);
+		
+		//check if game is finished
+		var gameOver = true;
+		//check row
+		for (j=1; j <=3; j++) {
+			if (row[currentRow][currentColumn] != row[currentRow][j]) {gameOver = false;}
+		}
+		if (gameOver===true) {alertFinish();}
+		//check column
+		for (i=1; i <=3; i++) {
+			if (row[currentRow][currentColumn] != row[i][currentColumn]) {gameOver = false;}
+		}
+		if (gameOver===true) {alertFinish();}
+		//check diagonal
+		if (row[1,1]===row[currentRow][currentColumn] && row[2,2]===row[currentRow][currentColumn] && row[3,3]===row[currentRow][currentColumn]) {alertFinish();}
+		if (row[1,3]===row[currentRow][currentColumn] && row[2,2]===row[currentRow][currentColumn] && row[3,1]===row[currentRow][currentColumn]) {alertFinish();}
+		//
+
 		XsTurn = !XsTurn;
 	});	
+}
+
+function alertFinish(){
+	alert("Game Over");
 }	
+	
