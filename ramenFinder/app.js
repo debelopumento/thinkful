fedc1dc6d43a45d29fd929e3781e944e5a6ec90c
debelopumento <imdiye@Gmail.com>
2017-01-05 06:45:42 -0800

    Added google map api

--- ramenFinder/app.js
+++ ramenFinder/app.js
@@ -1,23 +1,35 @@
+console.log(20);
+
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -35, lng: 150},
+          zoom: 15
+        });
+      }
+
 $(function() {
-	console.log("1");
-	watchSubmit()	
+    console.log("1");
+    watchSubmit()   
 });
 
+var latN;
+var lngN;
 
 function watchSubmit() {
-	console.log("2");
-	$('.js-search-form').submit(function(){
-		console.log("3");
-		event.preventDefault();
-		var userInputSearchLocation = $(this).find('.js-userInputSearchLocation').val();
-		console.log(13, userInputSearchLocation);
-		getResult(userInputSearchLocation);
-	});
+    console.log("2");
+    $('.js-search-form').submit(function(){
+        console.log("3");
+        event.preventDefault();
+        var userInputSearchLocation = $(this).find('.js-userInputSearchLocation').val();
+        console.log(13, userInputSearchLocation);
+        getResult(userInputSearchLocation);
+    });
 }
 
 
 function getResult (userInputSearchLocation) {
-	            function cb(data) {        
+                function cb(data) {        
                     console.log("cb: " + JSON.stringify(data));
             }
                 var auth = {
@@ -76,17 +88,35 @@ function getResult (userInputSearchLocation) {
 
 
 function renderBusinesses(data) {
-		var row = '';
-		var businessNum = data.businesses.length;
-		console.log(data);
-		for (var i = 0; i < businessNum; i++) {
-			var biz = data.businesses[i];
-			row += '<h4>' + biz.name + '</h4>';
+        var row = '';
+        var businessNum = data.businesses.length;
+        console.log(data);
+        for (var i = 0; i < businessNum; i++) {
+            var biz = data.businesses[i];
+            row += '<h4>' + biz.name + '</h4>';
             row += '<p>Yelp Rating: ' + biz.rating + '</p>';
             row += '<p>Phone number: ' + biz.display_phone + '</p>';
             row += '<p>Address: </p>';
             row += '<p>' + biz.location.display_address + '</p>';
-		}
-		$('.js-search-results').html(row);	
-}
 
+            //initMap(biz);
+           
+            //row += '<div id="map"></div>';
+            //row += '<script async defer rc="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAtjZoeYGWfzt4nJ2VM8Hs7NZzhN0wVgI&callback=initMap"></script>';
+            latN = biz.location.coordinate.latitude;
+            lngN = biz.location.coordinate.longitude;
+            //row += '<a href="map.html">google map</a>';
+            //row += '<div id="map"></div>';
+            //row += '<script>';
+            //row += 'var map;';
+            //row += 'function initMap() {';
+            //row += 'map = new google.maps.Map(document.getElementById("map"), {';
+            //row += 'center: {lat: -34.397, lng: 150.644},';
+            //row += 'zoom: 8';
+            //row += '});} </script>';
+            //row += '<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAtjZoeYGWfzt4nJ2VM8Hs7NZzhN0wVgI&callback=initMap" async defer></script>';
+
+
+        }
+        $('.js-search-results').html(row);  
+}
