653f87e8c5e5b0d13052451b2325a780bad67663
Di Ye <imdiye@gmail.com>
2017-01-17 12:43:27 -0800

    added search criteria listener

--- logoSearch/app.js
+++ logoSearch/app.js
@@ -1,9 +1,25 @@
-function renderLogos(logoLib, searchCri) {
+function refreshLib(programTypeCri, educationLevelCri, logoLib) {
+	
+}
+
+
+function renderLogos(logoLib) {
+	//render logos
 	var row = '';
 	logoLib.map(function(currentLogo) {
-		row += '<span class="logoUnit"><img src="' + currentLogo.url + '"/></span>';
+		if (currentLogo.fade===true) {
+			row += '<span class="logoUnit fade"><img src="' + currentLogo.url + '"/></span>';
+		}
+			else {row += '<span class="logoUnit"><img src="' + currentLogo.url + '"/></span>';}
 	});
 	$('.js-logoContainer').html(row);
+	//serach criteria changes
+	$('.js-searchBut').click(function(event){
+		var programTypeCri = $('#programType').find("option:selected").text();
+		var educationLevelCri = $('#educationLevel').find("option:selected").text();
+		console.log(1, programTypeCri, educationLevelCri);
+		refreshLib(programTypeCri, educationLevelCri, logoLib);
+	});
 }
 
 function init() {
@@ -11,47 +27,54 @@ function init() {
 	var logoLib = [
 		{	url: "images/aci.png",
 			programType: "scholarship",
-			educationLevel: "highSchool"
+			educationLevel: "highSchool",
+			fade: true
 		},
 		{	url: "images/acm.png",
 			programType: "loanProgram",
-			educationLevel: "highSchool"
+			educationLevel: "highSchool",
+			fade: true
+
 		},
 		{	url: "images/AFE.png",
 			programType: "loanProgram",
-			educationLevel: "kindergarden"
+			educationLevel: "kindergarden",
+			fade: true
 		},
 		{	url: "images/aiq.png",
 			programType: "scholarship",
-			educationLevel: "highSchool"
+			educationLevel: "highSchool",
+			fade: true
 		},
 		{	url: "images/google.png",
 			programType: "loanProgram",
-			educationLevel: "highSchool"
+			educationLevel: "highSchool",
+			fade: true
 		},
 		{	url: "images/skype.png",
 			programType: "scholarship",
-			educationLevel: "highSchool"
+			educationLevel: "highSchool",
+			fade: true
 		},
 		{	url: "images/slack.png",
 			programType: "loanProgram",
-			educationLevel: "kindergarden"
+			educationLevel: "kindergarden",
+			fade: true
 		},
 		{	url: "images/spartan.png",
 			programType: "loanProgram",
-			educationLevel: "kindergarden"
+			educationLevel: "kindergarden",
+			fade: true
 		},
 		{	url: "images/twitter.png",
 			programType: "scholarship",
-			educationLevel: "highSchool"
+			educationLevel: "highSchool",
+			fade: true
 		}
 	];
 	//
-	var searchCri = {
-		programType: [],
-		educationLevel: []
-	};
-	renderLogos(logoLib, searchCri);
+	
+	renderLogos(logoLib);
 }
 
 $(function() {
